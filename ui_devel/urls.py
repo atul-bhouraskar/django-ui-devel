44938666f1ea26835919149d29374d5aa239cab1
Atul Bhouraskar <atul@yescourse.com>
2016-05-23 20:04:53 -0400

    Upgrade urls.py to use list of urls instead of pattern

--- ui_devel/urls.py
+++ ui_devel/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import *
+from django.conf.urls import url
 
 from . import views
 
-urlpatterns = patterns('',
+urlpatterns = (
     url(r'^$', views.show_dashboard, name='show-dashboard'),
     url(r'^render-template/$', views.render_template, name='render-template'),
 )
